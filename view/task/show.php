3067b9cdb60faf51762d908ba6ea6a66d27869f1
Anais-RV <anaisrv@gmail.com>
2023-07-23 23:02:12 +0200

    Format table show

--- view/task/show.php
+++ view/task/show.php
@@ -4,18 +4,27 @@ require_once("./../../controllers/tasksController.php");
 
 $obj = new TaskController();
 $date = $obj->show($_GET['id']);
-print_r($date)
 ?>
 
 <table class="table container-fluid">
     <thead>
         <tr>
             <th scope="col">ID</th>
+            <th scope="col">TAREA</th>
+            <th scope="col">DESCRIPCIÓN</th>
+            <th scope="col">FECHA DE CREACIÓN</th>
+            <th scope="col">FECHA DE FINALIZACIÓN</th>
+            <th scope="col">ESTADO</th>
         </tr>
     </thead>
     <tbody>
         <tr>
             <td scope="col"><?= $date["id"] ?></td>
+            <td scope="col"><?= $date["Title"] ?></td>
+            <td scope="col"><?= $date["Description"] ?></td>
+            <td scope="col"><?= $date["Created_at"] ?></td>
+            <td scope="col"><?= $date["Finished_at"] ?></td>
+            <td scope="col"><?= $date["Completed"] ?></td>
         </tr>
     </tbody>
 </table>
